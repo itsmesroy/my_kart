af355025b6a95dbedca0d91cf6801e0bf5cb81e1
itsmesroy <itsmevicky78@gmail.com>
2024-11-07 11:37:27 +0530

    Reset nutton OK

--- src/App.js
+++ src/App.js
@@ -1,67 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar';
-import Products from './components/Products';
-import Bodyimg from './components/body';
-import Footer from './components/Footer';
-import React,{useState} from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Products from "./components/Products";
+import Bodyimg from "./components/body";
+import Footer from "./components/Footer";
+import React, { useState } from "react";
 
 function App() {
-  const productList=[{
-    price: 50,
-    name: "Spring Roll",
-    quantity: 0,
-  }, 
-  {
-    price: 99,
-    name: "Pizza",
-    quantity: 0,
-  }, 
-  {
-    price: 30,
-    name: "Pastries",
-    quantity: 0,
-  } ,
-  {
-    price: 25,
-    name: "Cold Drinks",
-    quantity: 0,
-  } 
-  ]
-let [products , setProducts]=useState(productList);
-let [totalAmount , setTotalAmount]=useState(0);
+  const productList = [
+    {
+      price: 50,
+      name: "Spring Roll",
+      quantity: 0,
+    },
+    {
+      price: 99,
+      name: "Pizza",
+      quantity: 0,
+    },
+    {
+      price: 30,
+      name: "Pastries",
+      quantity: 0,
+    },
+    {
+      price: 25,
+      name: "Cold Drinks",
+      quantity: 0,
+    },
+  ];
+  let [products, setProducts] = useState(productList);
+  let [totalAmount, setTotalAmount] = useState(0);
 
-const incrementQuantity=(index)=>{
-let newProducts=[...products];
-let newTotalamount= totalAmount;
-newProducts[index].quantity++;
-newTotalamount+=products[index].price;
-setTotalAmount(newTotalamount);
-setProducts(newProducts);
-}
-
-const decrementQuantity=(index)=>{
-  let newProducts=[...products];
-  let newTotalAmount=totalAmount;
-  if(newProducts[index].quantity>0){  
-    newProducts[index].quantity--;
-    newTotalAmount-=products[index].price;
-    
-  }
-  setTotalAmount(newTotalAmount);
+  const incrementQuantity = (index) => {
+    let newProducts = [...products];
+    let newTotalamount = totalAmount;
+    newProducts[index].quantity++;
+    newTotalamount += products[index].price;
+    setTotalAmount(newTotalamount);
     setProducts(newProducts);
-}
+  };
+
+  const decrementQuantity = (index) => {
+    let newProducts = [...products];
+    let newTotalAmount = totalAmount;
+    if (newProducts[index].quantity > 0) {
+      newProducts[index].quantity--;
+      newTotalAmount -= products[index].price;
+    }
+    setTotalAmount(newTotalAmount);
+    setProducts(newProducts);
+  };
+
+const handleReset=()=>{
+const resetProducts= products.map(product=>({...product,quantity:0}));
+setProducts(resetProducts);
+setTotalAmount(0);
+ };
+ 
   return (
-  <>
-    <NavBar/>
-     <main className='container mt-5'>
-      <Products productList={products} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity}/>
-     </main>
-     {/* <Bodyimg/> */}
-    <Footer totalAmount={totalAmount}/>  
-  </>
-  
+    <>
+      <NavBar />
+      <main className="container mt-5">
+        <Products
+          productList={products}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+        />
+      </main>
+      {/* <Bodyimg/> */}
+      <Footer totalAmount={totalAmount} handleReset={handleReset}/>
+    </>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,36 @@
-import React from 'react'
+import React from "react";
 
 class Footer extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="row ">
+          <button className="btn btn-danger col-2" onClick={this.props.handleReset}>
+            Reset
+          </button>
+          <div className="col-8 text-center">
+            Total Bill: ₹ {this.props.totalAmount}
+          </div>
+          <button className="btn btn-primary col-2">Pay Now</button>
+        </div>
 
- handleReset=()=>{
-        
-    this.props.setProducts(this.props.productList.map(product =>({ ...product,  quantity:0})))
-    this.props.setTotalAmount(0);
-    window.location.href('/');
-     
-        }
-    state = {  } 
-    render() {  
-        return (
-          <div>
-            <div className='row '>              
-                <button className='btn btn-danger col-2' onClick={this.handleReset}>Reset</button>            
-                <div className='col-8 text-center'>Total Bill: ₹ {this.props.totalAmount}</div>
-                <button className='btn btn-primary col-2'>Pay Now</button>  
-              </div>  
-
-<div className="card ">
-  <div className="card-header text-center mt-4 fixed-bottom">@FlourWish</div>
-  <div className="card-body fixed-bottom">
-    <blockquote className="blockquote mb-0 fixed-bottom">
-      <p>Explore the Best Bakery in Town</p>
-      <footer className="blockquote-footer fixed-bottom">
-           by Sroweta Kar 
-      </footer>
-    </blockquote>
-  </div>
-</div>
-            </div>
-
-
-        );
-    }
+        <div className="card ">
+          <div className="card-header text-center mt-4 fixed-bottom">
+            @FlourWish
+          </div>
+          <div className="card-body fixed-bottom">
+            <blockquote className="blockquote mb-0 fixed-bottom">
+              <p>Explore the Best Bakery in Town</p>
+              <footer className="blockquote-footer fixed-bottom">
+                by Sroweta Kar
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Footer;
